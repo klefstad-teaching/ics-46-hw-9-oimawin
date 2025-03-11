53bcfd45c5d16cc9ca445f84917b240dd8252a4a
Emma Huynh <emmath@uci.edu>
2025-03-11 06:18:20 +0000

    ladder load_words()

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -3,6 +3,11 @@
 #include "dijkstras.h"
 #include "ladder.h"
 
-TEST(Replace, Me) {
-  EXPECT_TRUE(false);
+string file_name = "/home/emmath/ics46/ics-46-hw-9-oimawin/src/words.txt";
+size_t word_count = 45392;
+
+TEST(WordLadder, load_words) {
+  set<string> word_list;
+  load_words(word_list, file_name);
+  EXPECT_EQ(word_list.size(), word_count);
 }
--- src/ladder.cpp
+++ src/ladder.cpp
@@ -4,3 +4,9 @@ void error(string word1, string word2, string msg) {
     cerr << "Error with word ladder [" << word1 << "->" << word2 << "]: " << msg << endl;
 }
 
+void load_words(set<string> & word_list, const string& file_name) {
+    ifstream in(file_name);
+    for (string word; in >> word;)
+        word_list.insert(word);
+    in.close();
+}
