4e8fcb4bd0c174e93cf6f3cd3f53ef2854c995f2
Emma Huynh <emmath@uci.edu>
2025-03-15 19:31:38 +0000

    Fixed gtests to capture stderr Added expected output strings

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -17,7 +17,8 @@ TEST(WordLadder, print_word_ladder) {
   testing::internal::CaptureStdout();
   print_word_ladder(ladder);
   string output = testing::internal::GetCapturedStdout();
-  EXPECT_EQ(output, "a -> b -> c -> d");
+  string expected = "a -> b -> c -> d";
+  EXPECT_EQ(output, expected);
 }
 
 TEST(WordLadder, valid_ladder_input_success) {
@@ -26,16 +27,24 @@ TEST(WordLadder, valid_ladder_input_success) {
   valid_ladder_input("ring", "worm", word_list);
 }
 
-TEST(WordLadder, valid_ladder_input_begin_end_same) {
+TEST(WordLadder, valid_ladder_input_begin_end_same_fail) {
   set<string> word_list;
   load_words(word_list, file_name);
+  testing::internal::CaptureStderr();
   valid_ladder_input("ring", "ring", word_list);
+  string output = testing::internal::GetCapturedStderr();
+  string expected = "[ERROR] word ladder \'ring -> ring\': Beginning and ending word are the same\n";
+  EXPECT_EQ(output, expected);
 }
 
 TEST(WordLadder, valid_ladder_input_end_word_fail) {
   set<string> word_list;
   load_words(word_list, file_name);
+  testing::internal::CaptureStderr();
   valid_ladder_input("ring", "s;ldkfja;lskdfj", word_list);
+  string output = testing::internal::GetCapturedStderr();
+  string expected = "[ERROR] word ladder \'ring -> s;ldkfja;lskdfj\': \'s;ldkfja;lskdfj\' is not a valid dictionary word\n";
+  EXPECT_EQ(output, expected);
 }
 
 TEST(WordLadder, is_adjacent) {
